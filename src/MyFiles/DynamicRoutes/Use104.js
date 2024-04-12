9f4fba5ad102b006f60fc0dec1b062fa13d16a9e
balunaidu <baluangular225@gmail.com>
2024-04-12 07:51:11 +0530

    usestate104 Re Pratices details page post2 component

--- src/MyFiles/DynamicRoutes/Use104.js
+++ src/MyFiles/DynamicRoutes/Use104.js
@@ -1,56 +1,54 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
-import {useParams,useNavigate} from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import Horizontalbar from "../Hooks/Chats/Horizontalbar";
 
 const Use104 = () =>{
 
- const {userId} = useParams();
- const [myList, setMyList] = useState([]);
+  const {userId} = useParams();
+  const [data, setData] = useState([]);
 
- const navigate = useNavigate();
+  const navigate = useNavigate();
 
- const fetchApiData = async (id) =>{
+  const fetchApiData = async (id) =>{
     try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-        const data = await response.json();
-        setMyList(data);
+      const responsive = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+      const data = await responsive.json();
+      setData(data);
     } catch (error) {
-        console.log('Error Message', error);
+      console.log('Error Message', error)
     }
- }
+  }
 
- useEffect(()=>{
-   fetchApiData(userId);
- },[userId])
+  useEffect(()=>{
+     fetchApiData(userId)
+  },[userId])
 
     return(
         <div>
             <Header/>
                <div className="container">
 
-                   <div className="row shadow p-3 mb-4 mt-4">
+                   <div className="row shadow p-5 mb-4 mt-4">
                     <div className="col-6 col-xs-12">
-                    <h3 className="mt-3 mb-3">Use104 Component</h3><hr/>
-                      {myList ? (
-                        <div className="col-12 col-xs-12">
-                            <div>
-                                <p><b>Name: </b>{myList.name}</p>
-                                <p><b>Email: </b>{myList.email}</p>
-                                <p><b>Website: </b>{myList.website}</p>
-                                <p><b>Phone: </b>{myList.phone}</p>
-                                <p><b>City: </b>{myList?.address?.city}</p>
-                                <button className="btn btn-primary rounded-0" onClick={()=> navigate(`/Usestate104`)}>Go Back</button>
-                            </div>
-                        </div>
-                      ):(
-                        <p>Use104 ALL Details Loading...</p>
-                      )}
+                      <h3>Use104 ALL Details </h3><hr/>
+                     {data ? (
+                      <div className="col-12 col-xs-12">
+                         <p><b>Name: </b>{data.name}</p>
+                         <p><b>Email: </b>{data.email}</p>
+                         <p><b>Website: </b>{data.website}</p>
+                         <p><b>Phone: </b>{data.phone}</p>
+                         <p><b>City: </b>{data?.address?.city}</p>
+                         <button className="btn btn-primary rounded-0" onClick={()=> navigate(`/Usestate104`)}>Go Back</button>
                       </div>
-                      <div className="col-6 col-xs-12">
-                        <Horizontalbar/>
-                      </div>
+                     ):(
+                      <p>Use104 Show All Details Loading...</p>
+                     )}
+                     </div>
+                     <div className="col-6 col-xs-12">
+                      <Horizontalbar/>
+                     </div>
                    </div>
 
                </div>
--- src/MyFiles/Hooks/Usepost2.js
+++ src/MyFiles/Hooks/Usepost2.js
@@ -4,53 +4,64 @@ import Footer from "../../Components/Footer";
 
 const Usepost2 = () =>{
 
- const [name, setName] = useState('');
- const [email, setEmail] = useState('');
- const [website, setWebsite] = useState('');
- const [errorForm, setErrorForm] = useState(false)
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [website, setWebsite] = useState('');
+    const [fromError, setFormError] = useState(false)
 
-//  console.log({name, email, website})
-const userDetails = {name, email, website}
+    // console.log(name, email, website);
 
-const formSubmit = async (e) =>{
-    e.preventDefault();
-    if(!name || !email || !website){
-        setErrorForm('ALL Input Fields or Required');
-        return;
+    const allData = {name, email, website};
+
+    const submitForm = async (e) =>{
+        e.preventDefault();
+
+        if(!name || !email || !website){
+            setFormError('ALL Input Fields or Required');
+
+            return;
+        }
+
+         // Email validation using regular expression
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(email)) {
+                setFormError('Please enter a valid email address');
+                return false; // Indicates validation failure
+            }
+
+        setFormError(false);
+
+        console.log(allData);
+
+        const responsive = await fetch('http://localhost:3333/Users',{
+            method:'POST',
+            headers:{
+                'Content-Type' : 'Application/json'
+            },
+            body: JSON.stringify(allData)
+        })
+        if(responsive.ok){
+            setName('');
+            setEmail('');
+            setWebsite('');
+            alert('Data save Successfully');
+        }else{
+            alert('Data Failed Successfully')
+        }
     }
-    setErrorForm(false);
-    console.log(userDetails);
-
-    const response = await fetch('http://localhost:3333/Users',{
-        method:'POST',
-        headers:{
-            'Content-Type' : 'Application/json'
-        },
-        body: JSON.stringify(userDetails)
-    })
-    if(response.ok){
-        setName('')
-        setEmail('');
-        setWebsite('');
-        alert('Data Save on API Successfully');
-    }else{
-        alert('Data Failed on API Successfully');
-    }
-    
-}
 
     return(
         <div>
             <Header/>
               <div className="container">
                   <div className="shadow p-3 mt-4 mb-4">
-                      <form onSubmit={formSubmit}>
-                          <input type="text" className="form-control mb-2" name="name" value={name} onChange={(e)=> setName(e.target.value)} />
-                          <input type="email" className="form-control mb-2" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
-                          <input type="text" className="form-control mb-2" name="website" value={website} onChange={(e)=> setWebsite(e.target.value)} />
-                          {errorForm && <p style={{color:'red'}}>{errorForm}</p>}
-                          <input type="submit" className="btn btn-primary mb-2" />
-                      </form>
+                     <form onSubmit={submitForm}>
+                        <input type="text" className="form-control mb-2" name="name" value={name} onChange={(e)=> setName(e.target.value)} />
+                        <input type="email" className="form-control mb-2" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+                        <input type="text" className="form-control mb-2" name="website" value={website} onChange={(e)=> setWebsite(e.target.value)} />
+                        {fromError && <p style={{color:'red'}}>{fromError}</p>}
+                        <input type="submit" className="btn btn-primary rounded-0 mb-2" />
+                     </form>
                   </div>
               </div>
             <Footer/>
